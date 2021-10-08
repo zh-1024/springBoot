eb230045cacb4aea16eae7c0566126f686f5090d
zh-1024 <1923991238@qq.com>
2021-10-08 18:06:38 +0800

    修改helloword

--- src/main/java/com/zh/web01/controller/HelloWord.java
+++ src/main/java/com/zh/web01/controller/HelloWord.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWord {
     @RequestMapping("/1.jpg")
     public String hello(){
+        System.out.println("hello");
         return "hello word";
     }
 }
